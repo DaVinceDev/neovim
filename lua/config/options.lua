08575f3d4014d5f071ec403ec3f662dd61c7d0a8
DaVinceDev <tokyh0008@gmail.com>
2025-07-10 12:43:31 +0100

    autopairs

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,4 +5,6 @@ vim.cmd([[highlight Normal guibg=NONE ctermbg=NONE]])
 vim.cmd([[highlight NormalNC guibg=NONE ctermbg=NONE]])
 vim.cmd([[highlight Pmenu guibg=NONE ctermbg=NONE]]) -- optional: popup menu
 vim.cmd([[highlight EndOfBuffer guibg=NONE ctermbg=NONE]])
-
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
