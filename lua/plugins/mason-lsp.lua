1c0e3bb53f14041237ba6ac64a9463f0198d0a57
upn3xt <whonextmail25@gmail.com>
2025-08-30 11:26:52 +0100

    dart

--- lua/plugins/mason-lsp.lua
+++ lua/plugins/mason-lsp.lua
@@ -1,3 +1,4 @@
+
 return {
   "williamboman/mason-lspconfig.nvim",
   dependencies = {
@@ -6,24 +7,45 @@ return {
   },
   config = function()
     require("mason").setup()
-
     require("mason-lspconfig").setup({})
 
     local lspconfig = require("lspconfig")
 
-  lspconfig.nil_ls.setup({
-    cmd = {"nil"},
-    filetypes = {"nix"},
-    settings = {
+    lspconfig.nil_ls.setup({
+      cmd = {"nil"},
+      filetypes = {"nix"},
+      settings = {
         nil_ls = {
-            formatting = {
-        command = {"nixpkgs-fmt"}  -- or "nixfmt" if you prefer
-      }
+          formatting = {
+            command = {"nixpkgs-fmt"}  -- or "nixfmt" if you prefer
+          }
         }
       }
-  })
+    })
+
+    lspconfig.dartls.setup({
+      cmd = { "dart", "language-server", "--protocol=lsp" },
+      filetypes = { "dart" },
+      root_dir = lspconfig.util.root_pattern("pubspec.yaml"),  -- Root directory setup
+      init_options = {
+        closingLabels = true,
+        flutterOutline = true,
+        onlyAnalyzeProjectsWithOpenFiles = true,
+        outline = true,
+        suggestFromUnimportedLibraries = true,
+      },
+      settings = {
+        dart = {
+          completeFunctionCalls = true,
+          showTodos = true,
+        },
+      },
+      on_attach = function(client, bufnr)
+        print("Dart LSP attached to buffer " .. bufnr)
+        -- Optionally set up key mappings here
+      end,
+    })
 
     local servers = require("mason-lspconfig")
   end,
 }
-
